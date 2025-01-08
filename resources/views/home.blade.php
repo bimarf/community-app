6d897fe63427baf59f85ad2bbdfeb87e1ce96334
bimarf <bimarenggaxvi@gmail.com>
2025-01-08 15:10:38 +0700

    help others section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,6 +87,113 @@
         </div>
     </section>
 
+    <section class="bg-gray">
+        <div class="container py-80px">
+            <h2 class="text-center mb-5">Help Others</h2>
+            <div class="row">
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card">
+                        <a href="#">
+                            <h3>How to add a custom validation in laravel?</h3>
+                        </a>
+                        <div>
+                            <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user
+                                roles, among which, the ...</p>
+                            <div class="row">
+                                <div class="col me-1 me-lg-2">
+                                    <a href="#">
+                                        <span class="badge rounded-pill text-bg-light">
+                                            Eloquent
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-5 col-lg-7">
+                                    <div class="avatar-sm-wrapper d-inline-block">
+                                        <a class="me-1" href="#">
+                                            <img src="{{ url('assets/images/avatar.png') }}"
+                                                class="avatar rounded-circle" alt="">
+                                        </a>
+                                    </div>
+                                    <span class="fs-12px">
+                                        <a class="me-1 fw-bold" href="#">Bima Rengga
+                                        </a>
+                                        <span class="color-gray text-nowrap">2 hours ago</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card">
+                        <a href="#">
+                            <h3>How to add a custom validation in laravel?</h3>
+                        </a>
+                        <div>
+                            <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user
+                                roles, among which, the ...</p>
+                            <div class="row">
+                                <div class="col me-1 me-lg-2">
+                                    <a href="#">
+                                        <span class="badge rounded-pill text-bg-light">
+                                            Eloquent
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-5 col-lg-7">
+                                    <div class="avatar-sm-wrapper d-inline-block">
+                                        <a class="me-1" href="#">
+                                            <img src="{{ url('assets/images/avatar.png') }}"
+                                                class="avatar rounded-circle" alt="">
+                                        </a>
+                                    </div>
+                                    <span class="fs-12px">
+                                        <a class="me-1 fw-bold" href="#">Bima Rengga
+                                        </a>
+                                        <span class="color-gray text-nowrap">2 hours ago</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card">
+                        <a href="#">
+                            <h3>How to add a custom validation in laravel?</h3>
+                        </a>
+                        <div>
+                            <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user
+                                roles, among which, the ...</p>
+                            <div class="row">
+                                <div class="col me-1 me-lg-2">
+                                    <a href="#">
+                                        <span class="badge rounded-pill text-bg-light">
+                                            Eloquent
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-5 col-lg-7">
+                                    <div class="avatar-sm-wrapper d-inline-block">
+                                        <a class="me-1" href="#">
+                                            <img src="{{ url('assets/images/avatar.png') }}"
+                                                class="avatar rounded-circle" alt="">
+                                        </a>
+                                    </div>
+                                    <span class="fs-12px">
+                                        <a class="me-1 fw-bold" href="#">Bima Rengga
+                                        </a>
+                                        <span class="color-gray text-nowrap">2 hours ago</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 </body>
 
